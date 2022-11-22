148170617daf857034400bf3a2883cd8f47df67f
reino <reino@DESKTOP-QB4VGSB>
2022-11-22 09:16:36 -0400

    Empezar evento

--- proyectofinal/src/Visual/RegEvento.java
+++ proyectofinal/src/Visual/RegEvento.java
@@ -7,10 +7,15 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class RegEvento extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JButton btnAgregar;
+	private JButton btnCancelar;
 
 	/**
 	 * Launch the application.
@@ -29,27 +34,62 @@ public class RegEvento extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegEvento() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Agregar evento");
+		setBounds(100, 100, 651, 587);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(12, 13, 609, 148);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("C\u00F3digo:");
+		lblNewLabel.setBounds(12, 13, 56, 16);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre:");
+		lblNewLabel_1.setBounds(12, 58, 56, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Cupo:");
+		lblNewLabel_2.setBounds(290, 13, 56, 16);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Ubicaci\u00F3n:");
+		lblNewLabel_3.setBounds(290, 58, 71, 16);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Fecha de inicio:");
+		lblNewLabel_4.setBounds(12, 98, 99, 16);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Fecha de conclusi\u00F3n:");
+		lblNewLabel_5.setBounds(290, 98, 128, 16);
+		panel.add(lblNewLabel_5);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				btnAgregar = new JButton("Agregar");
+				btnAgregar.setActionCommand("OK");
+				buttonPane.add(btnAgregar);
+				getRootPane().setDefaultButton(btnAgregar);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
 			}
 		}
 	}
-
 }
