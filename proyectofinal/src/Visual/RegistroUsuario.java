433c087c33a79351d07d108b5d5de0b90931dfb7
reino <reino@DESKTOP-QB4VGSB>
2022-12-04 17:04:01 -0400

    Registro terminado

--- proyectofinal/src/Visual/RegistroUsuario.java
+++ proyectofinal/src/Visual/RegistroUsuario.java
@@ -30,13 +30,16 @@ public class RegistroUsuario extends JDialog {
 	private JTextField txtUsuario;
 	private JTextField txtContrasena;
 	private JComboBox boxTipo;
+	private JTextField txtValidarcontra;
+	private JLabel Contraanterior;
+	private JTextField txtContraseñaant;
 
 	/**
 	 * Launch the application.
 	 *//*
 	public static void main(String[] args) {
 		try {
-			RegistroUsuario dialog = new RegistroUsuario();
+			RegistroUsuario dialog = new RegistroUsuario(0);
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -49,7 +52,7 @@ public class RegistroUsuario extends JDialog {
 	 */
 	public RegistroUsuario(int opcion) {
 		setTitle("Registrar Usuario");
-		setBounds(100, 100, 338, 284);
+		setBounds(100, 100, 372, 310);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLocationRelativeTo(null);
@@ -64,7 +67,7 @@ public class RegistroUsuario extends JDialog {
 			if(opcion == 0)
 				btnRegistrar = new JButton("Registrar");
 			else
-				btnRegistrar = new JButton("Modificar");
+				btnRegistrar = new JButton("Modificar");	
 			btnRegistrar.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					if(opcion == 0)
@@ -73,7 +76,7 @@ public class RegistroUsuario extends JDialog {
 						modificar();
 				}
 			});
-			btnRegistrar.setBounds(41, 172, 97, 25);
+			btnRegistrar.setBounds(59, 199, 97, 25);
 			panel.add(btnRegistrar);
 			
 			btnCancelar = new JButton("Cancelar");
@@ -82,7 +85,7 @@ public class RegistroUsuario extends JDialog {
 					dispose();
 				}
 			});
-			btnCancelar.setBounds(161, 172, 97, 25);
+			btnCancelar.setBounds(175, 199, 97, 25);
 			panel.add(btnCancelar);
 			
 			JLabel lblNewLabel = new JLabel("Usuario:");
@@ -90,15 +93,28 @@ public class RegistroUsuario extends JDialog {
 			panel.add(lblNewLabel);
 			
 			JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a:");
-			lblNewLabel_1.setBounds(22, 71, 82, 16);
+			if(opcion == 0)
+				lblNewLabel_1.setBounds(22, 70, 82, 16);
+			else
+				lblNewLabel_1.setBounds(22, 113, 82, 16);
 			panel.add(lblNewLabel_1);
 			
-			JLabel lblNewLabel_2 = new JLabel("Tipo de usuario:");
-			lblNewLabel_2.setBounds(22, 119, 105, 16);
-			panel.add(lblNewLabel_2);
+			
+			if(opcion == 0)
+			{
+				JLabel lblNewLabel_2 = new JLabel("Tipo de usuario:");
+				lblNewLabel_2.setBounds(22, 156, 105, 16);
+				panel.add(lblNewLabel_2);
+				
+				boxTipo = new JComboBox();
+				boxTipo.setModel(new DefaultComboBoxModel(new String[] {"<Seleccionar>", "Administrador", "Secretaria"}));
+				boxTipo.setBounds(165, 155, 167, 19);
+				panel.add(boxTipo);
+			}
+				
 			
 			txtUsuario = new JTextField();
-			txtUsuario.setBounds(122, 24, 157, 22);
+			txtUsuario.setBounds(165, 24, 167, 22);
 			panel.add(txtUsuario);
 			txtUsuario.setColumns(10);
 			if(opcion == 1)
@@ -107,31 +123,72 @@ public class RegistroUsuario extends JDialog {
 				txtUsuario.setText(EventoCiencia.getInstance().getUser().getUser());
 			}
 				
-			
 			txtContrasena = new JTextField();
-			txtContrasena.setBounds(122, 68, 157, 22);
+			if(opcion == 0)
+				txtContrasena.setBounds(165, 67, 167, 22);
+			else
+				txtContrasena.setBounds(165, 110, 167, 22);
 			panel.add(txtContrasena);
 			txtContrasena.setColumns(10);
 			
-			boxTipo = new JComboBox();
-			boxTipo.setModel(new DefaultComboBoxModel(new String[] {"<Seleccionar>", "Administrador", "Secretaria"}));
-			boxTipo.setBounds(122, 118, 157, 19);
-			panel.add(boxTipo);
+			JLabel lblNewLabel_3 = new JLabel("Confirmar contrase\u00F1a:");
+			if(opcion == 0)
+				lblNewLabel_3.setBounds(22, 113, 134, 16);
+			else
+				lblNewLabel_3.setBounds(22, 156, 134, 16);
+			panel.add(lblNewLabel_3);
+			
+			txtValidarcontra = new JTextField();
+			if(opcion == 0)
+				txtValidarcontra.setBounds(165, 110, 167, 22);
+			else
+				txtValidarcontra.setBounds(165, 155, 167, 22);
+			panel.add(txtValidarcontra);
+			txtValidarcontra.setColumns(10);
+			
+			if(opcion == 1)
+			{
+				Contraanterior = new JLabel("Contrase\u00F1a anterior:");
+				Contraanterior.setBounds(22, 70, 134, 16);
+				panel.add(Contraanterior);
+				
+				txtContraseñaant = new JTextField();
+				txtContraseñaant.setBounds(165, 67, 167, 22);
+				panel.add(txtContraseñaant);
+				txtContraseñaant.setColumns(10);
+			}	
 		}
 	}
 	
 	public void registrar() {
-		Usuario aux = new Usuario(boxTipo.getSelectedItem().toString(), txtUsuario.getText(), txtContrasena.getText());
-		EventoCiencia.getInstance().reguser(aux);
-		JOptionPane.showMessageDialog(null, "Usuario registrado", "Informacion", JOptionPane.INFORMATION_MESSAGE);
-		clear();
+		if(txtContrasena.getText().equals(txtValidarcontra))
+		{
+			Usuario aux = new Usuario(boxTipo.getSelectedItem().toString(), txtUsuario.getText(), txtContrasena.getText());
+			EventoCiencia.getInstance().reguser(aux);
+			JOptionPane.showMessageDialog(null, "Usuario registrado", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+			clear();
+		}
+		else
+			JOptionPane.showMessageDialog(null, "Las contraseñas no son iguales", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+		
 	}
 	
 	public void modificar() {
-		Usuario aux = EventoCiencia.getInstance().buscarusuario(txtUsuario.getText());
-		aux.setContrasena(txtContrasena.getText());
-		JOptionPane.showMessageDialog(null, "Contraseña modificada", "Informacion", JOptionPane.INFORMATION_MESSAGE);
-		dispose();
+		if(EventoCiencia.getInstance().confirmLogin(txtUsuario.getText(), txtContraseñaant.getText()))
+		{
+			if(txtContrasena.getText().equals(txtValidarcontra))
+			{
+				Usuario aux = EventoCiencia.getInstance().buscarusuario(txtUsuario.getText());
+				aux.setContrasena(txtContrasena.getText());
+				JOptionPane.showMessageDialog(null, "Contraseña modificada", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+				dispose();
+			}
+			else
+				JOptionPane.showMessageDialog(null, "Las contraseñas no son iguales", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+		}
+		else
+			JOptionPane.showMessageDialog(null, "Contraseña incorrecta", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+		
 	}
 	
 	public void clear() {
